9c1f06cacc131384159023ecae36dd260e33d4ee
dbera17 <dbera17@freeuni.edu.ge>
2025-04-03 19:57:24 +0400

    React app setup, started Client side application

---
+++ ev-store-client/README.md
@@ -0,0 +1,5 @@
+# EV Adapter Store
+Welcome to the **EV Adapter Store**, your one-stop web app for browsing and purchasing electric vehicle adapters and charging stations. This project is built with React and is designed to provide a smooth and intuitive shopping experience for EV enthusiasts.
+
+## Getting Started
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ ev-store-client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Your Store is a web application that allows users to browse and purchase products online.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.yourstore.ge/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ready to update this old app!!!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
