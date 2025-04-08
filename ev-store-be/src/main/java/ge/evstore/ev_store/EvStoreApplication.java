92c702d1d32790a59b39460ee765bb2c053ec9a5
DoomSlayer <dgogi17@freeuni.edu.ge>
2025-04-08 15:05:00 +0400

    auth first commit. needs jwts dependency

--- ev-store-be/src/main/java/ge/evstore/ev_store/EvStoreApplication.java
+++ ev-store-be/src/main/java/ge/evstore/ev_store/EvStoreApplication.java
@@ -2,12 +2,20 @@ package ge.evstore.ev_store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @SpringBootApplication
 public class EvStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EvStoreApplication.class, args);
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationManagerBuilder authenticationManagerBuilder) {
+		return authenticationManagerBuilder.getOrBuild();
 	}
 
 }
