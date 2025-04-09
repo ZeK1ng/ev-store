d32c01ec0551157022c509c5240b10eacf280bca
DoomSlayer <dgogi17@freeuni.edu.ge>
2025-04-10 00:06:14 +0400

    Working on authentication configuration

--- ev-store-be/src/main/java/ge/evstore/ev_store/controller/AuthenticationController.java
+++ ev-store-be/src/main/java/ge/evstore/ev_store/controller/AuthenticationController.java
@@ -2,11 +2,10 @@ package ge.evstore.ev_store.controller;
 
 import ge.evstore.ev_store.response.AuthRequest;
 import ge.evstore.ev_store.response.AuthResponse;
+import ge.evstore.ev_store.service.AuthService;
 import ge.evstore.ev_store.utils.JwtUtils;
 import ge.evstore.ev_store.utils.TokenType;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,13 +15,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.Map;
-
 @Controller
 @Slf4j
 public class AuthenticationController {
-    @Autowired
-    private AuthenticationManager authManager;
+    private final AuthenticationManager authManager;
+    private final AuthService authService;
+
+    public AuthenticationController(AuthenticationManager authManager, AuthService authService) {
+        this.authManager = authManager;
+        this.authService = authService;
+    }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
@@ -33,8 +35,8 @@ public class AuthenticationController {
         UserDetails user = (UserDetails) auth.getPrincipal();
 
         String accessToken = JwtUtils.generateToken(user, TokenType.ACCESS_TOKEN);
-        String refreshToken =  JwtUtils.generateToken(user, TokenType.REFRESH_TOKEN);
-
+        String refreshToken = JwtUtils.generateToken(user, TokenType.REFRESH_TOKEN);
+        authService.registerTokensForUser(user,accessToken,refreshToken);
         return ResponseEntity.ok(new AuthResponse(accessToken, refreshToken));
     }
 
---
+++ ev-store-be/src/main/java/ge/evstore/ev_store/service/AuthService.java
@@ -0,0 +1,7 @@
+package ge.evstore.ev_store.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+}
