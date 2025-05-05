603bdbad7ed2c853207ce7fe2f8f552b73d6546c
DoomSlayer <dgogi17@freeuni.edu.ge>
2025-05-05 22:38:35 +0400

    stash

--- db init.sql
+++ db init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "products" (
   "id" integer PRIMARY KEY,
   "name" varchar2,
-  "decription" varchar2,
+  "description" varchar2,
   "price" double,
   "stock_amount" integer,
   "category_id" integer,
@@ -13,17 +13,18 @@ CREATE TABLE "users" (
   "name" varchar2,
   "surname" varchar2,
   "email" varchar2,
-  "salt" varchar2,
   "passwordHash" varchar2,
   "phone_number" varchar2,
   "address" varchar2,
-  "role" varchar2
+  "role" varchar2,
+  "favouriteItems" products[]
 );
 
 CREATE TABLE "category" (
   "id" integer PRIMARY KEY,
   "name" varchar2,
-  "description" varchar2
+  "description" varchar2,
+  "parentCategoryId" integer
 );
 
 CREATE TABLE "images" (
@@ -33,6 +34,25 @@ CREATE TABLE "images" (
   "file_path" varchar2
 );
 
+CREATE TABLE "authTokens" (
+  "id" integer PRIMARY KEY,
+  "user" users,
+  "accessToken" varchar2,
+  "refreshToken" varchar2
+);
+
+CREATE TABLE "dictionary" (
+  "id" integer,
+  "key" varchar2,
+  "value" varchar2
+);
+
 ALTER TABLE "products" ADD CONSTRAINT "product_category" FOREIGN KEY ("category_id") REFERENCES "category" ("id");
 
 ALTER TABLE "products" ADD CONSTRAINT "product_image" FOREIGN KEY ("image_id") REFERENCES "images" ("id");
+
+ALTER TABLE "authTokens" ADD CONSTRAINT "refreshToken_user" FOREIGN KEY ("user") REFERENCES "users" ("id");
+
+ALTER TABLE "users" ADD CONSTRAINT "favourites" FOREIGN KEY ("favouriteItems") REFERENCES "products" ("id");
+
+ALTER TABLE "category" ADD CONSTRAINT "category_subCatergory" FOREIGN KEY ("parentCategoryId") REFERENCES "category" ("id");
