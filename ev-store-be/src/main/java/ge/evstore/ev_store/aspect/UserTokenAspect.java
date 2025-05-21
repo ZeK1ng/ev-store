319e6c06813f9156326691cdf30f795cd31452fd
DoomSlayer <dgogi17@freeuni.edu.ge>
2025-05-21 22:26:03 +0400

    add userTokenAspect for nonAdmins

--- ev-store-be/src/main/java/ge/evstore/ev_store/aspect/UserTokenAspect.java
+++ ev-store-be/src/main/java/ge/evstore/ev_store/aspect/UserTokenAspect.java
@@ -30,7 +30,7 @@ public class UserTokenAspect {
     }
 
     @Around("execution(* ge.evstore.ev_store.service.interf.AdminService.*(..))")
-    public Object aroundMethodsForTokens(final ProceedingJoinPoint pjp) throws Throwable {
+    public Object aroundMethodsForTokensForAdmin(final ProceedingJoinPoint pjp) throws Throwable {
         final Object[] args = pjp.getArgs();
         final int n = args.length;
         String accessToken = (String) args[n - 2];
@@ -49,6 +49,42 @@ public class UserTokenAspect {
             log.error("Access denied. User is not Admin");
             throw new AccessDeniedException("User is not Admin");
         }
+        final boolean accessExpired = jwtUtils.isTokenExpired(accessToken);
+        // Rotate both tokens if only the access token is expired
+        if(!authService.validTokens(accessToken,refreshToken)){
+            log.error("Access denied. Tokens not valid");
+            throw new AccessDeniedException("Tokens not valid");
+        }
+        if (accessExpired) {
+            final String username = jwtUtils.extractUsername(refreshToken);
+            final UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            accessToken = jwtUtils.generateToken(userDetails, TokenType.ACCESS_TOKEN);
+            authService.rotateAccessTokenForUser(username, accessToken);
+            // replace the last two args
+            args[n - 2] = accessToken;
+            args[n - 1] = refreshToken;
+        }
+
+        // Proceed with (possibly) new tokens
+        return pjp.proceed(args);
+    }
+    //TODO
+//    @Around("")
+    public Object aroundMethodsForTokensForUser(final ProceedingJoinPoint pjp) throws Throwable {
+        final Object[] args = pjp.getArgs();
+        final int n = args.length;
+        String accessToken = (String) args[n - 2];
+        final String refreshToken = (String) args[n - 1];
+        if (accessToken == null || refreshToken == null || accessToken.isEmpty() || refreshToken.isEmpty()) {
+            log.error("Access token or refresh token is empty");
+            throw new AccessDeniedException("Access denied. Missing Token");
+        }
+        final boolean refreshExpired = jwtUtils.isTokenExpired(refreshToken);
+        if (refreshExpired) {
+            log.error("Access denied. Refresh token expired");
+            throw new AccessDeniedException("Both access and refresh tokens are expired");
+        }
+
         final boolean accessExpired = jwtUtils.isTokenExpired(accessToken);
         // Rotate both tokens if only the access token is expired
         if(!authService.validTokens(accessToken,refreshToken)){
