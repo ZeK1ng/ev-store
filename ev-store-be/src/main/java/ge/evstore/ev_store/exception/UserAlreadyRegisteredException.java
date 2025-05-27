f9761c55fadbdc130ee25568d4921cce8c397681
DoomSlayer <dgogi17@freeuni.edu.ge>
2025-05-27 17:06:25 +0400

    fix log.

--- ev-store-be/src/main/java/ge/evstore/ev_store/exception/UserAlreadyRegisteredException.java
+++ ev-store-be/src/main/java/ge/evstore/ev_store/exception/UserAlreadyRegisteredException.java
@@ -4,16 +4,15 @@ import ge.evstore.ev_store.request.UserRegisterRequest;
 
 public class UserAlreadyRegisteredException extends RuntimeException {
 
-    public UserAlreadyRegisteredException(UserRegisterRequest userRegisterRequest) {
+    public UserAlreadyRegisteredException(final UserRegisterRequest userRegisterRequest) {
         super(constructMessage(userRegisterRequest));
 
     }
 
-    private static String constructMessage(UserRegisterRequest userRegisterRequest) {
+    private static String constructMessage(final UserRegisterRequest userRegisterRequest) {
         return "User already registered with this email address: " +
                 userRegisterRequest.getEmail() +
-                "name and surname: " +
-                userRegisterRequest.getFirstName() + " " + userRegisterRequest.getLastName() +
-                "\n";
+                ". Requested name and surname: " +
+                userRegisterRequest.getFirstName() + " " + userRegisterRequest.getLastName();
     }
 }
