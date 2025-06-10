ebc87a7b9cd3490a46d0487f6b3f507595b73335
DoomSlayer <dgogi17@freeuni.edu.ge>
2025-06-10 23:28:22 +0400

    migrate id to productId in ProductResponse

--- ev-store-be/src/main/java/ge/evstore/ev_store/response/ProductResponse.java
+++ ev-store-be/src/main/java/ge/evstore/ev_store/response/ProductResponse.java
@@ -13,7 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class ProductResponse {
-    private Long id;
+    private Long productId;
     private String nameGE;
     private String nameENG;
     private String nameRUS;
@@ -39,7 +39,7 @@ public class ProductResponse {
 
     public static ProductResponse from(final Product product, final List<Long> imageIds) {
         return ProductResponse.builder()
-                .id(product.getId())
+                .productId(product.getId())
                 .nameGE(product.getNameGE())
                 .nameENG(product.getNameENG())
                 .nameRUS(product.getNameRUS())
