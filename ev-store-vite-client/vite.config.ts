cf1cd60c55e094a6369a6ccde9b2bac71bb9b60d
dbera17 <dbera17@freeuni.edu.ge>
2025-06-12 17:03:04 +0400

    add new api url

--- ev-store-vite-client/vite.config.ts
+++ ev-store-vite-client/vite.config.ts
@@ -2,19 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from "vite-tsconfig-paths"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   optimizeDeps: {
     include: ['@chakra-ui/icons', '@chakra-ui/react']
-  },
-  server: {
-    proxy: {
-      '/api/v1/image': {
-        target: 'http://variance-betty-these-breach.trycloudflare.com',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
   }
 })
